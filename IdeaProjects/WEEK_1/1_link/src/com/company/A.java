e58370db3e9353508ee6ab0cb0648716556f82bc
bagdatzhumagazieva <bagdon.zhumagazieva@gmail.com>
2018-09-10 22:30:13 +0600

    labWork1

---
+++ IdeaProjects/WEEK_1/1_link/src/com/company/A.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+public class A {
+    public static void main(String[] args) {
+
+        Scanner cs=new Scanner(System.in);
+        int a=cs.nextInt();
+        int b=cs.nextInt();
+        double c=Math.sqrt(a*a+b*b);
+        System.out.print(c);
+    }
+}
+ 
---
+++ IdeaProjects/WEEK_1/1_link/src/com/company/D.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+public class D {
+    public static void main(String[] args) {
+
+        Scanner cs=new Scanner(System.in);
+        int a=cs.nextInt();
+        int b=cs.nextInt();
+        int c=b%a;
+        System.out.print(c);
+    }
+}
+ 
